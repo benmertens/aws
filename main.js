57ebefca0daf3210b02c6cd9826d2072d78d8c09
benmertens <ben.mertens@student.uibk.ac.at>
2025-05-05 20:35:08 +0200

    aws starten

---
+++ main.js
@@ -0,0 +1,66 @@
+/* Wetterstationen Euregio Beispiel */
+
+// Innsbruck
+let ibk = {
+    lat: 47.267222,
+    lng: 11.392778,
+    zoom: 11,
+};
+
+// Karte initialisieren
+let map = L.map("map").setView([ibk.lat, ibk.lng], ibk.zoom);
+
+// thematische Layer
+let overlays = {
+    stations: L.featureGroup().addTo(map),
+}
+
+// Layer control
+L.control.layers({
+    "Relief avalanche.report": L.tileLayer(
+        "https://static.avalanche.report/tms/{z}/{x}/{y}.webp", {
+        attribution: `© <a href="https://sonny.4lima.de">Sonny</a>, <a href="https://www.eea.europa.eu/en/datahub/datahubitem-view/d08852bc-7b5f-4835-a776-08362e2fbf4b">EU-DEM</a>, <a href="https://lawinen.report/">avalanche.report</a>, all licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>`
+    }).addTo(map),
+    "OpenStreetMap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+}, {
+    "Wetterstationen": overlays.stations,
+}).addTo(map);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+// Wetterstationen
+async function loadStations(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+
+    // Wetterstationen mit Icons und Popups
+    L.geoJSON(jsondata, {
+        attribution: "Datenquelle: <a href='https://data.wien.gv.at'> Stadt Wien </a>",
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: "icons/wifi.png",
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            });
+        },
+        // Popup
+        onEachFeature: function (feature, layer) {
+            //console.log(feature.properties);
+            layer.bindPopup(`
+                <img src="${feature.properties.THUMBNAIL}" alt="*">
+                <h4>${feature.properties.NAME}<h4>
+                <adress>${feature.properties.ADRESSE}</adress>
+                <a href="${feature.properties.WEITERE_INF}" target="wien">Webseite</a>    
+                `);
+        }
+    }).addTo(overlays.stations);
+}
+
+loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
