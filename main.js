20f7658b1cbd0983dce41d0b4d7f2a315cde8ad9
benmertens <ben.mertens@student.uibk.ac.at>
2025-05-08 16:08:32 +0200

    verbessert

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ async function loadStations(url) {
             layer.bindPopup(`
                 <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4> 
                 <ul>
-                    <li>Lufttemperatur (C) ${feature.properties.LT || "-"}</li>
+                    <li>Lufttemperatur (C) ${feature.properties.LT !== undefined ? feature.properties.LT : "-"}</li>
                     <li>Relative Luftfeuchte (%) ${feature.properties.RH || "-"}</li>
                     <li>Windgeschwindigkeit (km/h) ${feature.properties.WG || "-"}</li>
                     <li>Schneehöhe (cm) ${feature.properties.HS || "-"}</li>
