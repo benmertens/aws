e5c2c16c63fa97bec2b052be7be141f871e33489
benmertens <ben.mertens@student.uibk.ac.at>
2025-05-15 16:11:01 +0200

    Pfeil aws div icon wind schwarz

--- main.js
+++ main.js
@@ -177,7 +177,7 @@ function showDirection(jsondata) {
             return L.marker(latlng, {
                 icon: L.divIcon({
                     className: "aws-div-icon-wind",
-                    html: `<span style="transform:rotate(${feature.properties.WR}deg);background-color:${color}"><i class="fa-solid fa-circle-arrow-down"></i>${degreetoText}</span>`
+                    html: `<span><i style="transform:rotate(${feature.properties.WR}deg);color:${color}"class="fa-solid fa-circle-arrow-down"></i></span>`
                 }),
             })
         },
